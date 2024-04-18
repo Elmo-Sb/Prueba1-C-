98d26d5b758d58905c5789523c7d3e438147c18c
forta <forta@RAULPC>
2024-04-17 21:13:54 -0500

    Agregar archivos de proyecto.

---
+++ Prueba1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+string one = "1. Agregar Producto";
+string two = "2. Inventario";
+string three = "3. Recursos Humanos";
+string four = "4. Exit";
+
+Console.WriteLine($"{one} \n{two} \n{three} \n{four} \nEscribe el número al que desees ingresar.");
+
+Console.ReadLine();
+
+Console.WriteLine("Por la we es aún no sirve");
