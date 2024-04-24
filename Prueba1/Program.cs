63975abc8f0d430ac979db51c3891334da3093e5
Raul Cuentas Gonzalez <raul.acg.sb@gmail.com>
2024-04-24 00:04:30 -0500

    Error en comentario

--- Prueba1/Program.cs
+++ Prueba1/Program.cs
@@ -1,12 +1,11 @@
-﻿using System.Reflection;
+﻿Console.WriteLine("---------------MENU---------------\n" +
+                  "1. Productos\n" +
+                  "2. Empleados\n" +
+                  "3. Salir\n" +
+                  "Seleccione una opcion\n" +
+                  "----------------------------------");
 
-string one = "1. Agregar Producto";
-string two = "2. Inventario";
-string three = "3. Recursos Humanos";
-string four = "4. Exit";
-
-Console.WriteLine($"{one} \n{two} \n{three} \n{four} \nEscribe el número al que desees ingresar.");
-
-Console.ReadLine();
-
-Console.WriteLine("Por la we es aún no sirve");
+public class MenuPrincipal
+{
+    private string input = Console.ReadLine();
+}
